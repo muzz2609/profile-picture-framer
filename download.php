288630c0859b4cac0a4a6f76ef9fd4940336c626
muzz2609 <87675200+muzz2609@users.noreply.github.com>
2021-08-30 00:08:57 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
   <head>
-    <title>Download Profile Picture | Support Kerala Blasters !</title>
+    <title>Download Profile Picture | #Vaxsquad </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/style.css" rel="stylesheet" async="async" />
   </head>
   <body>
     <div id="wrapper">
       <div id="content">
-        <h1><a href="http://demos.sim/isl-profile-pic/">Support Kerala Blasters!</a></h1>
-        <p>Make this as your profile picture to support Blasters !</p>
+        <h1><a href="http://demos.sim/isl-profile-pic/">Thanks for joining #vaxsquad</a></h1>
+        <p>Make this your profile picture to spread the word!</p>
         <?php
         $url = htmlspecialchars($_GET["i"]);
         if(isset($_GET["i"]))
@@ -18,10 +18,10 @@
           header("Redirect: index.php");
         ?>
         <p>
-        Click the above image to download.<br/>Or right click (in mobile, hold down) and choose "Save Image"
+        Click the image above to download it.<br/>Or hold down on the image and choose "Save Image"
         </p>
         <p>
-          <a href="index.php"><button id="download">Create Another Profile Picture!</button></a>
+          <a href="index.php"><button id="download">Create Another Vaccine Profile Picture!</button></a>
         </p>
         <?php
         require_once __DIR__ . "/footer.php";
